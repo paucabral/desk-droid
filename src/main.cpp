19fd7011f014f599cead6efaf0fbedf28e020664
paucabral <josepaucabral@gmail.com>
2025-12-27 14:41:03 +0800

    feat: initiate fritzing schematics

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
+#include <CuteBuzzerSounds.h>
 
 // OLED VARS
 #define i2c_Address        0x3c
@@ -10,6 +11,9 @@
 #define OLED_SCREEN_WIDTH  128 // OLED display width, in pixels
 #define OLED_SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET         -1   //   QT-PY / XIAO
+
+// BUZZER VARS
+#define BUZZER_PIN         11
 
 Adafruit_SH1106G display = Adafruit_SH1106G(OLED_SCREEN_WIDTH, OLED_SCREEN_HEIGHT, &Wire1, OLED_RESET);
 
@@ -27,9 +31,13 @@ void setup()   {
 
   roboEyes.setAutoblinker(ON, 3, 2);
   roboEyes.setIdleMode(ON, 2, 2);
+
+  cute.init(BUZZER_PIN);
+  cute.play(S_CONNECTION);
 }
 
 
 void loop() {
  roboEyes.update(); // update eyes drawings
+ //cute.play(S_MODE1);
 }
