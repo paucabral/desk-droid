419fb54c995272691a31eda28ba29e7c8e1dd8f1
paucabral <josepaucabral@gmail.com>
2025-12-27 14:16:17 +0800

    feat: Add test code for roboeyes

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
-#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
 
-void setup() {
-  Serial.begin(12500);
+// OLED VARS
+#define i2c_Address        0x3c
+#define OLED_SCL           13
+#define OLED_SDA           12
+#define OLED_SCREEN_WIDTH  128 // OLED display width, in pixels
+#define OLED_SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET         -1   //   QT-PY / XIAO
+
+Adafruit_SH1106G display = Adafruit_SH1106G(OLED_SCREEN_WIDTH, OLED_SCREEN_HEIGHT, &Wire1, OLED_RESET);
+
+#include <FluxGarage_RoboEyes.h>
+RoboEyes<Adafruit_SH1106G> roboEyes(display); // create RoboEyes instance
+
+void setup()   {
+  Serial.begin(115200);
+
+  delay(250);
+  Wire1.begin(OLED_SDA, OLED_SCL);
+  display.begin(i2c_Address, true);
+
+  roboEyes.begin(OLED_SCREEN_WIDTH, OLED_SCREEN_HEIGHT, 100);
+
+  roboEyes.setAutoblinker(ON, 3, 2);
+  roboEyes.setIdleMode(ON, 2, 2);
 }
 
+
 void loop() {
-  Serial.println("Hello, World!");
-  delay(1000);
+ roboEyes.update(); // update eyes drawings
 }
